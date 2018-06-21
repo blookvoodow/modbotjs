94c17de0346757cb86ca641557e56ebea4354aec
yessiree <mgpuff1@hotmail.com>
2018-06-21 15:48:43 -0400

    removed some scaffolding

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import { add, multiply } from './sample';
-import * as _ from 'lodash';
-console.log('sample app');
-console.log('1 + 1 is ', add(1, 1));
-console.log('3 * 4 is ', multiply(3, 4));
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-let processedNumbers = _.filter(
-	_.map(numbers, x => x * 2),
-	x => x % 2 === 0);
+import { DonBot } from './donbot';
 
-console.log(processedNumbers);
+let bot = DonBot({
+    username: 'yessiree',
+    password: 'YOUR SECRET KEY'
+});
+
+bot.init().then(() => {
+    bot.getUserId();
+});
