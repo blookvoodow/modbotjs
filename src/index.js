30148acd9f1aee5e687ac080d48dda33ea53a772
mike <mikeguo1992@gmail.com>
2021-05-15 18:13:57 +0800

    start on current page without posting

--- src/index.js
+++ src/index.js
@@ -11,15 +11,18 @@ var id;
 
 async function main() {
     let bot = modbot({threadUrl: '/viewtopic.php?f=51&t=86578'})
+    await bot.login();
+    console.log('Logged in.')
     let pageOfNextPost;
-    let currPage = 17;
+    const posts = await bot.getNumberOfPosts()
+    let currPage = Math.ceil(posts/25);
+    console.log('current page set to', currPage)
 
     const pollUpdate = async () => {
-        console.log(new Date().toString())
         try {
             const posts = await bot.getNumberOfPosts()
             pageOfNextPost = Math.ceil((posts + 1) / pageSize)
-            console.log('current posts:', posts, 'current page:', currPage, 'page of next post:', pageOfNextPost)
+            console.log('total posts:', posts, 'current page:', currPage, 'page of next post:', pageOfNextPost)
             
             if (pageOfNextPost > currPage) {
                 console.log('making a post...')
@@ -37,8 +40,6 @@ async function main() {
         }
     }
 
-    await bot.login();
-    console.log('Logged in.')
     id = setInterval(pollUpdate, 600000);
 }
 
