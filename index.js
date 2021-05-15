410279dc513bfb690761da1d6665453225dcc3b5
mike <mikeguo1992@gmail.com>
2021-05-15 15:40:32 +0800

    dynamic port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const main = require('./src/index.js')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const username = process.env.MAFIASCUM_USERNAME;
